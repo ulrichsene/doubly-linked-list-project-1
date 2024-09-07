ac10808d88c9ed23592e74539bc215fdd9be2ec3
Scipio Hutchison <ahutchison@zagmail.gonzaga.edu>
2024-09-07 11:58:59 -0700

    modified:   unit_tests.cpp

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -2,9 +2,7 @@
 #include <cassert>
 #include "linked_list.hpp"
 
-
 using namespace std;
-
 
 bool test_insert() {
    // set up
@@ -17,7 +15,6 @@ bool test_insert() {
    bool return3 = lst.insert(1, 30);
    bool return4 = lst.insert(1, 20);
 
-
    // validation
    assert(return0 == false);
    assert(return1 == true);
@@ -28,26 +25,87 @@ bool test_insert() {
    assert(lst.get_head()->next->tree_data == 20);
    assert(lst.get_head()->next->next->tree_data == 30);
 
-
    // cleanup
    // deconstructor deletes dynamically allocated memory
    return true;
 }
 
-
 bool test_remove() {
+   // set up
    LinkedList lst;
    lst.insert(0, 10);
    lst.insert(1, 20);
    lst.insert(2, 30);
    lst.insert(3, 100);
 
+   // execution
    bool return1 = lst.remove(2);
    bool return2 = lst.remove(0);
    bool return3 = lst.remove(4);
 
+   // validation
+   assert(return1 == true);
+   assert(return2 == true);
+   assert(return3 == false);
 
+   // cleanup
+   // deconstructor deletes dynamically allocated memory
    return true; 
+}
+
+bool test_size() {
+   // set up
+   LinkedList lst; // list with elements
+   LinkedList lst2; // empty list
+   // execution
+   lst.insert(0, 10);
+   lst.insert(1, 20);
+   lst.insert(2, 30);
+   // validation
+   assert(lst.size() == 3);
+   assert(lst2.size() == 0);
+   // cleanup
+   return true;
+}
+bool test_print_forward() {
+   // set up 
+   LinkedList lst; 
+   // execution
+   lst.insert(0, 10);
+   lst.insert(1, 20);
+   lst.insert(2, 30);
+   // validation
+   lst.print_forward();
+   // cleanup
+   return true;
+}
+
+bool test_print_backward() {
+   // set up 
+   LinkedList lst; 
+   // execution
+   lst.insert(0, 10);
+   lst.insert(1, 20);
+   lst.insert(2, 30);
+   // validation
+   lst.print_backward();
+   // cleanup
+   return true;
+}
+
+bool test_push_front() {
+   // set up 
+   LinkedList lst;
+   // execution
+   lst.push_front(30);
+   lst.push_front(20);
+   lst.push_front(10);
+   // validation
+   assert(lst.get_head()->tree_data == 10);
+   assert(lst.get_head()->next->tree_data == 20);
+   assert(lst.get_head()->next->next->tree_data == 30);
+   // cleanup
+   return true;
 }
 
 int main(){
